4f1efaaf41f61879cfb75582e7ec5c81a209946e
Миша Чукарев <mishakrut11@gmail.com>
2022-10-23 21:37:44 +0400

    add lineterminator

--- first.py
+++ first.py
@@ -5,7 +5,7 @@ import get_way
 
 def create_annotation(subdir):
     with open("annotation.csv", mode="a", encoding='utf-8') as w_file:
-        file_writer = csv.writer(w_file, delimiter = ";")
+        file_writer = csv.writer(w_file, delimiter = ";",lineterminator='\n')
         file_writer.writerow(["Абсолютный путь", "Относительный путь", "Класс"])
         for i in range(1020) :
             if (os.path.isfile(get_way.get_absolute_way(subdir, i, "download")) == True ):
--- fourth.py
+++ fourth.py
@@ -2,12 +2,12 @@ from Iterator import iterator
 
 
 def main():
-        print('Начало')
-        val = iterator("annotation.csv", "cat")
-        for way in val:
-            print(way)
-        print('Конец')
-
-
+    print('Начало')
+    val = iterator('annotation.csv', 'cat')
+    for way in val:
+        print(way)
+    print('Конец')
+    
+    
 if __name__ == "__main__":
-        main()
+    main()
